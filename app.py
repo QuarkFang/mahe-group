30c4e7cae9dba58bae5d357559bc06c8772a67d2
QuarkFang <1220804359@qq.com>
2018-06-29 01:34:19 +0800

    test

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+app.config['BOOTSTRAP_SERVE_LOCAL'] = True
+Bootstrap(app)
+
+web_name = 'Northeastern University Ma He Group'
+
+@app.route('/')
+def index():
+    return render_template('home.html', title_name=web_name)
+
+
+@app.route('/home/')
+def home():
+    return render_template('home.html', title_name=web_name)
+
+
+@app.route('/Leader/')
+def leader():
+    return render_template('leader.html', title_name=web_name)
+
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", debug=True, port=8080)
